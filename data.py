09bca42f8ec35714a4993c1ad52bdd39d5b0dfed
NiWi <73991577+Frid0l1n@users.noreply.github.com>
2022-05-10 08:20:47 +0200

    added functionality to data*

--- data.py
+++ data.py
@@ -13,6 +13,17 @@ class data:
         #calculate the change in price using the python diff function
         price_data["Change In Price"] = price_data["Close"].diff()
 
+        n = 14
+
+        low_14 = price_data[['Low']].copy()
+        high_14 = price_data[['High']].copy()
+        low14 = low_14['Low'].transform(lambda x: x.rolling(window = n).min())
+        high14 = high_14['High'].transform(lambda x: x.rolling(window = n).max())
+        price_data['Low14'] = low14
+        price_data['High14'] = high14
+
+        price_data = price_data.dropna()
+
         print(price_data)
 
 x = input("enter stock: ")

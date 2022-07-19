cde42b4ce8bf2aa9828ecae7f142392826d1490f
Frid0l1n <73991577+Frid0l1n@users.noreply.github.com>
2022-07-19 18:07:15 +0100

    moin

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import yfinance as yf
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 from data import data
-import openpyxl as xls
 
 
 stock = input("enter stock: ")
@@ -14,8 +14,6 @@ start = input("enter start date: ")
 end = input("enter end date: ")
 
 data = data(stock, start, end)
-
-print(data.price_data)
 
 
 #create trainingset
@@ -31,19 +29,9 @@ print(data.price_data)
 X = data.price_data[["Low","High","Close","Volume","Change In Price", "RSI","Low14","High14"]]
 y = data.price_data[["Close"]]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X, y = make_classification(n_samples=60, n_features=4, n_informative=2, n_redundant=0, random_state=2, shuffle=False)
 
-regressor = RandomForestRegressor(n_estimators=4, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
+Classifier = RandomForestClassifier(n_estimators=4, random_state=0)
+Classifier.fit(X,y)
+y_pred = Classifier.predict(X)
 print(y_pred)
-
-#convert ndray to dataframe
-print(type(y_pred))
-df = pd.DataFrame(y_pred, columns= ["Prediction"])
-print(type(df))
-
-#write the output to excel file
-excel_file = pd.ExcelWriter("prediction.xlsx")
-df.to_excel(excel_file)
-excel_file.save()

33176496da7acb40991cf288f731ee944e670e55
Frid0l1n <73991577+Frid0l1n@users.noreply.github.com>
2022-07-29 16:52:05 +0100

    message

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import accuracy_score
-import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from data import data
